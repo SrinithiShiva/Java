4ae3920511f5480948f11c671773d11be153d230
SrinithiShiva <147691313+SrinithiShiva@users.noreply.github.com>
2025-09-13 19:37:39 +0530

    Delete utils directory

--- utils/ManagementUtil.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import java.util.function.Predicate;
-
-public class ManagementUtil {
-    public static final Predicate<String> isNullOrEmpty = (str) -> str == null || str.trim().isEmpty();
-    public static final Predicate<String> isMailChecked = (email) -> isNullOrEmpty.and((str-> email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"))).test(email);
-    public static final Predicate<String> isValidPhoneNumber = (phoneNumber) -> isNullOrEmpty.and(phone -> phone.matches("^\\+?[0-9]{10,13}$")).test(phoneNumber);
-}
